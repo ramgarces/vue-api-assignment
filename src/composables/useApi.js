7f2b1dd9506044f8a898f67747ccdbf8bd2c2fff
Ramiro Garces <ramgarces24@gmail.com>
2023-03-07 22:51:57 -0600

    Change API call to https

--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { ref } from 'vue'
 
 const instance = axios.create({
-    baseURL: 'http://acnhapi.com/v1/'
+    baseURL: 'https://acnhapi.com/v1/'
 })
 
 const useApi = () => {
